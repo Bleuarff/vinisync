b81c6510e39d2d4067f4d8333524234ae82c0c4e
bleuarff <bleuarff@gmail.com>
2020-04-18 00:40:02 +0200

    Complete model in ui

--- src/Entry.svelte
+++ src/Entry.svelte
@@ -25,6 +25,7 @@ let entry = {
 let edit = false
 let saved = false
 
+let cepageText = entry.wine.cepages.join(', ') //intermediate for cepage text input values
 $: serialized = JSON.stringify(entry)
 
 onMount(async () => {
@@ -35,11 +36,29 @@ onMount(async () => {
     entry = await repo.getEntry(params.id)
 })
 
+
 async function save(){
-  await repo.addEntry(entry)
-  saved = true
+  try{
+    sanitizeEntry()
+    if (params.id){
+      // TODO update
+    }
+    else //save
+      await repo.addEntry(entry)
+
+    saved = true
+  }
+  catch(ex){
+    // TODO show error
+  }
+
 }
 
+function sanitizeEntry(){
+  entry.wine.cepages = cepageText.split(',').filter(c => !!c).map(c => c.trim())
+
+  // TODO: check for mandatory fields
+}
 
 </script>
 
@@ -58,6 +77,42 @@ async function save(){
   de <input type="text" bind:value={entry.wine.apogeeStart}> à
   <input type="text" bind:value={entry.wine.apogeeEnd}>
   <label>Bouteilles</label><input type="number" bind:value={entry.count}>
+
+  <label>Cepages</label>
+    {#if edit}
+      <input type="text" bind:value={cepageText} placeholder="muscat, sauvignon, malbec">
+    {:else}
+      {#each entry.wine.cepages as cepage}
+        <span>{cepage}</span>
+      {/each}
+    {/if}
+
+  <label>Contenance</label>
+  {#if edit}
+    <input type="text" v:bind={entry.wine.containing} placeholder="0.75">
+  {:else}
+    <span>{entry.wine.containing}</span>
+  {/if}
+
+  <label>Couleur</label>
+  {#if edit}
+    <input type="text" v:bind={entry.wine.color} placeholder="rouge">
+  {:else}
+    <span>{entry.wine.color}</span>
+  {/if}
+
+  <div id="attrs">
+    <input type="checkbox" v:bind={entry.wine.sweet} id="sweet"><label for="sweet">Moelleux, Liquoreux</label>
+    <input type="checkbox" v:bind={entry.wine.sparkling} name="sparkling"><label for="sparkling">Pétillant</label>
+  </div>
+
+  <label>Emplacement</label>
+  {#if edit}
+    <input type="text" v:bind={entry.location} placeholder="rouge">
+  {:else}
+    <span>{entry.location}</span>
+  {/if}
+
 
   {#if edit}
     <div>

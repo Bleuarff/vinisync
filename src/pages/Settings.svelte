61f4ad97fa1fb544fc58b433fdb365cb871395ca
bleuarff <bleuarff@gmail.com>
2020-10-20 14:58:35 +0200

    Profile info in settings page

--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -1,8 +1,25 @@
 <script>
+  import {onMount} from 'svelte'
   import Download from '../components/Download.svelte'
   import LocationManager from '../components/LocationManager.svelte'
   import { createEventDispatcher } from 'svelte'
   const dispatch = createEventDispatcher();
+  import { DateTime } from 'luxon'
+
+  let email
+  let createDate
+
+  onMount(() => {
+    try{
+      const user = JSON.parse(localStorage.getItem('user'));
+      email = user.email
+
+      createDate = DateTime.fromISO(user.createDate).setLocale(document.documentElement.lang)
+    }
+    catch(ex){
+      console.error(ex)
+    }
+  })
 
   function forward(event) {
 		dispatch('notif', event.detail);
@@ -14,3 +31,35 @@
 <Download></Download>
 
 <LocationManager on:notif={forward}></LocationManager>
+
+<div id="profile">
+  <h2>Profil</h2>
+    <!-- TODO: make it editable -->
+    <label>email</label>
+    <span>{email}</span>
+
+    <label>Créé le</label>
+    <span>{createDate && createDate.toLocaleString(DateTime.DATE_HUGE)} à {createDate && createDate.toLocaleString(DateTime.TIME_24_SIMPLE)}</span>
+</div>
+
+<style>
+  #profile{
+    margin: 4em -.5em 2em -.5em;
+    border-radius: 3px;
+    border: 1px solid var(--dark-blue);
+    padding: 1em;
+  }
+
+  #profile span{
+    display: inline-block;
+    margin-left: 1em;
+    margin-bottom: .7em;
+  }
+
+  label{
+    font-size: .9em;
+  }
+  h2{
+    margin-bottom: .8em;
+  }
+</style>

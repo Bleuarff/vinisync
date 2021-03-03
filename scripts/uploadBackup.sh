51193c5f3ccfc2796b2430bd6ab6122b4dfbd735
bleuarff <bleuarff@gmail.com>
2021-03-03 23:59:23 +0100

    Add script to upload file to scaleway s3

---
+++ scripts/uploadBackup.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Uploads a file to vni-backups bucket
+
+echo "creds $SCW_ACCESS_KEY/$SCW_SECRET_KEY" # check credentials exist
+yyyymmdd=`date +%Y%m%d`
+s3Bucket="vni-backups"
+bucketLocation="fr-par"
+s3SecretKey=${SCW_SECRET_KEY}
+s3AccessKey=${SCW_ACCESS_KEY}
+endpoint="${s3Bucket}.s3.fr-par.scw.cloud"
+fileName="hello.txt"
+contentLength=`cat ${fileName} | wc -c`
+contentHash=`openssl dgst -sha256 -hex ${fileName} | sed 's/.* //'`
+contentType=`file -b --mime-type $fileName`
+echo $contentType
+b64=`openssl md5 -binary "$fileName" | openssl base64`
+acl="private"
+
+date=`date -u +%Y%m%dT%H%M%SZ`
+expdate_s="2022-12-30T12:00:00.000Z"
+region="fr-par"
+
+p=$(cat <<POLICY | openssl base64 | tr -d \\n
+{ "expiration": "${expdate_s}T12:00:00.000Z",
+  "conditions": [
+    {"acl": "$acl" },
+    {"bucket": "$s3Bucket" },
+    ["starts-with", "\$key", ""],
+    {"x-amz-date": "$date" },
+    {"x-amz-credential": "${s3AccessKey}/${yyyymmdd}/${region}/s3/aws4_request" },
+    {"x-amz-algorithm": "AWS4-HMAC-SHA256" }
+  ]
+}
+POLICY
+)
+
+stringToSign=$p
+echo "----------------- canonicalRequest --------------------"
+echo -e ${canonicalRequest}
+echo "----------------- stringToSign --------------------"
+echo -e ${stringToSign}
+echo "-------------------------------------------------------"
+
+# calculate the signing key
+DateKey=`echo -n "${yyyymmdd}" | openssl dgst -sha256 -hex -hmac "AWS4${s3SecretKey}" | sed 's/.* //'`
+DateRegionKey=`echo -n "${bucketLocation}" | openssl dgst -sha256 -hex -mac HMAC -macopt hexkey:${DateKey} | sed 's/.* //'`
+DateRegionServiceKey=`echo -n "s3" | openssl dgst -sha256 -hex -mac HMAC -macopt hexkey:${DateRegionKey} | sed 's/.* //'`
+SigningKey=`echo -n "aws4_request" | openssl dgst -sha256 -hex -mac HMAC -macopt hexkey:${DateRegionServiceKey} | sed 's/.* //'`
+# then, once more a HMAC for the signature
+signature=`echo -en ${p} | openssl dgst -sha256 -hex -mac HMAC -macopt hexkey:${SigningKey} | sed 's/.* //'`
+
+key_and_sig_args="-F X-Amz-Credential=${s3AccessKey}/${yyyymmdd}/${region}/s3/aws4_request -F X-Amz-Algorithm=AWS4-HMAC-SHA256 -F X-Amz-Signature=$signature -F X-Amz-Date=${date}"
+#-F "X-Amz-Security-Token= ${xamztoken}" \
+
+curl -v   \
+-F key=$fileName \
+-F acl=$acl \
+$key_and_sig_args  \
+-F "content-md5= ${b64}" \
+-F "Policy=$p" \
+-F "file=@$fileName" \
+https://${endpoint}

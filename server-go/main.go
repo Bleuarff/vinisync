2008992bd1849733f531aacfbc04cbb125afc3bf
bleuarff <bleuarff@gmail.com>
2024-01-17 23:35:31 +0100

    Add graceful shutdown
    Taken from https://echo.labstack.com/docs/cookbook/graceful-shutdown

--- server-go/main.go
+++ server-go/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"time"
 
@@ -25,7 +28,23 @@ func main() {
 	// Routes
 	e.GET("/api/ping", Ping)
 
-	e.Logger.Fatal(e.Start(":5136"))
+	go func() {
+		if err := e.Start(":5136"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println("Server shutting down")
 }
 
 func Ping(c echo.Context) error {
